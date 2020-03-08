6c94a84d53ad7ed5e821ae09679448c203809839
Danielle <bakerdd@lafayette.edu>
2020-03-07 20:41:44 -0500

    SignUp updated

--- chamafrontend/src/App.js
+++ chamafrontend/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SignIn from './routes/SignIn.js'
+import SignUp from './routes/SignUp.js'
 function App() {
   return (
     <div>
         <SignIn />
+        <SignUp />
     </div>
   );
 }
